7ea5cc2422c1ce0f45266c5df6ae6d3cadb0d07a
Sergey <xxsmotur@gmail.com>
2016-09-10 18:22:43 +0500

    init for the port scan

---
+++ portscan.go
@@ -0,0 +1,54 @@
+package main
+
+// portscan provides multi scanning of the ports
+// each port at the separate goroutine
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"time"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	minPortParam = kingpin.Flag("minport", "Scanning from this port").Default("0").Int()
+	maxPortParam = kingpin.Flag("maxport", "Scanning to this port").Default("65535").Int()
+	hostParam    = kingpin.Flag("host", "target host").Required().String()
+)
+
+func scan(host string, minPort, maxPort int) {
+	var (
+		wg sync.WaitGroup
+	)
+
+	wg.Add(maxPort - minPort)
+	for i := minPort; i < maxPort; i++ {
+		go func(port int) {
+			addr := fmt.Sprintf("%s:%d", host, i)
+			conn, err := net.DialTimeout("tcp", addr, 1*time.Second)
+			if err != nil {
+				wg.Done()
+				fmt.Printf("Error: %v", err)
+				return
+			}
+			wg.Done()
+			err = conn.Close()
+			if err != nil {
+				fmt.Println("Error: %v", err)
+			}
+		}(i)
+	}
+}
+func main() {
+	kingpin.CommandLine.Help = "Simple tool for scanning ports"
+	kingpin.Parse()
+	if *minPortParam > *maxPortParam {
+		fmt.Println("maxport should be greater then minport")
+		os.Exit(1)
+	}
+
+	scan(*hostParam, *minPortParam, *maxPortParam)
+}
